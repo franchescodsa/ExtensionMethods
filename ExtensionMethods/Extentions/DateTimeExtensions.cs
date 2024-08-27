ada721947c25a2c3fe20f2efd279f522786f41be
franchesco aprigio <franchescodsa@outlook.com>
2024-08-27 09:33:28 -0300

    Adicionar arquivos de projeto.

---
+++ ExtensionMethods/Extentions/DateTimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods.Extentions
+{
+    static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisobj)
+        {
+            TimeSpan durations = DateTime.Now.Subtract(thisobj);
+            if (durations.TotalHours <24.0)
+            {
+                return durations.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + "hours";
+            }
+            else
+            {
+                return durations.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + "days";
+            }
+
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using ExtensionMethods.Extentions;
+
+namespace ExtensionMethods
+{
+    internal class Program
+    {
+        /*Vamos criar umextensionmethod chamado "ElapsedTime()" no struct
+ DateTime para apresentar um objeto DateTime na forma de tempo
+ decorrido, podendo ser emhoras (semenor que24h) ouemdias caso
+ contrário.Porexemplo:
+ DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+ Console.WriteLine(dt.ElapsedTime());
+ "4.5 hours"
+ "3.2 days"
+*/
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2024, 8, 25, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
